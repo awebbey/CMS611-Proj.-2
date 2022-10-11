8734c3aa4cb3e842aa211484d7f2da68ad76267e
Adam Bundy <halaffamagician@gmail.com>
2022-10-10 20:57:23 -0400

    LineRenderer Dumb + Collision Detection

--- Ghost Chef Platformer/Assets/LineRendererScript.cs
+++ Ghost Chef Platformer/Assets/LineRendererScript.cs
@@ -10,9 +10,9 @@ public class LineRendererScript : MonoBehaviour
     Camera camera;
     LineRenderer lr;
 
-    Vector3 camOffset = new Vector3(0, 0, 10);
+    Vector3 camOffset = new Vector3(0, 0, 25);
 
-    [SerializeField] AnimationCurve ac;
+    //[SerializeField] AnimationCurve ac;
 
     // Start is called before the first frame update
     void Start()
@@ -23,29 +23,75 @@ public class LineRendererScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+
+        if (Input.GetMouseButton(0))
         {
             if (lr == null)
             {
                 lr = gameObject.AddComponent<LineRenderer>();
+                lr.enabled = true;
+                lr.positionCount = 2;
+                lr.startWidth = 0.1f;
+                lr.endWidth = 0.1f;
+                //lr.startColor = (1, 1, 1, 0);
+                //lr.endColor = (.5, .5, .5, .9);
+                startPos = camera.ScreenToWorldPoint(Input.mousePosition) + camOffset;
+                lr.SetPosition(0, startPos);
             }
-            lr.enabled = true;
-            lr.positionCount = 2;
-            startPos = camera.ScreenToWorldPoint(Input.mousePosition) + camOffset;
-            lr.SetPosition(0, startPos);
+                //Debug.Log(camera.ScreenToWorldPoint(Input.mousePosition));
+            if (lr.enabled == false)
+            { 
+                lr.enabled = true;
+                lr.positionCount = 2;
+                startPos = camera.ScreenToWorldPoint(Input.mousePosition) + camOffset;
+                lr.SetPosition(0, startPos);
+                //lr.width = 0.1;
+                //lr.color = (255, 255, 255);
+            }
+            //startPos = camera.ScreenToWorldPoint(Input.mousePosition) + camOffset;
+            //lr.SetPosition(0, startPos);
             lr.useWorldSpace = true;
-            lr.widthCurve = ac;
-            lr.numCapVertices = 10;
-            if (Input.GetMouseButton(0))
-            {
-                endPos = camera.ScreenToWorldPoint(Input.mousePosition) + camOffset;
-                lr.SetPosition(1, endPos);
-                //lr.SetPosition(startPos, endPos);
-            }
-            if (Input.GetMouseButtonUp(0))
+
+            //lr.widthCurve = ac;
+            //lr.numCapVertices = 10;
+            endPos = camera.ScreenToWorldPoint(Input.mousePosition) + camOffset;
+            lr.SetPosition(1, endPos);
+
+        }
+        else
+        {
+            if (lr != null)
             {
                 lr.enabled = false;
             }
         }
     }
+    //void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        //Debug.Log(camera.ScreenToWorldPoint(Input.mousePosition));
+    //        if (lr == null)
+    //        {
+    //            lr = gameObject.AddComponent<LineRenderer>();
+    //        }
+    //        lr.enabled = true;
+    //        lr.positionCount = 2;
+    //        startPos = camera.ScreenToWorldPoint(Input.mousePosition) + camOffset;
+    //        lr.SetPosition(0, startPos);
+    //        lr.useWorldSpace = true;
+    //        //lr.widthCurve = ac;
+    //        //lr.numCapVertices = 10;
+    //        if (Input.GetMouseButton(0))
+    //        {
+    //            endPos = camera.ScreenToWorldPoint(Input.mousePosition) + camOffset;
+    //            lr.SetPosition(1, endPos);
+    //            //lr.SetPosition(startPos, endPos);
+    //        }
+    //        if (Input.GetMouseButtonUp(0))
+    //        {
+    //            lr.enabled = false;
+    //        }
+    //    }
+    //}
 }
